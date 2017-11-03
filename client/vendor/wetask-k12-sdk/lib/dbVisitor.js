2a9dc7c048a3aa54b9375d5e77dcf95d1ef0998b
zy <yzhang@tcomchina.com>
2017-11-03 08:37:07 +0800

    c

--- client/vendor/wetask-k12-sdk/lib/dbVisitor.js
+++ client/vendor/wetask-k12-sdk/lib/dbVisitor.js
@@ -2,20 +2,6 @@
 *   数据访问接口
 */
 
-
-/**
- * @class
- * 一份作业 (容器)
- *
- */
-var TaskBlock = function TaskBlock(id, name, folder) {
-  this.id = id;
-  this.name = name;
-  this.folder = folder;
-  this.courses = new Array();
-};
-
-
 /**
  * @class
  * 作业文件夹
@@ -24,6 +10,21 @@ var TaskBlock = function TaskBlock(id, name, folder) {
 var TaskFolder = function TaskFolder(id, name) {
   this.id = id;
   this.name = name;
+};
+
+/**
+ * @class
+ * 一份作业 (容器)
+ *
+ */
+var TaskBlock = function TaskBlock(id, name, folderId, folderName) {
+  this.id = id;
+  this.name = name;
+
+  this.folderId = folderId;
+  this.folderName = folderName;
+
+  this.courses = new Array();
 };
 
 /**
@@ -43,9 +44,18 @@ var Course = function Course(id, name) {
  * 作业项
  *
  */
-var TaskItem = function TaskItem(id, title) {
+var TaskItem = function TaskItem(id, title, folderId, folderName, courseId, courseName, blockId, blockName) {
   this.id = id;
   this.title = title;
+
+  this.folderId = folderId;
+  this.folderName = folderName;
+
+  this.courseId = courseId;
+  this.courseName = courseName;
+
+  this.blockId = blockId;
+  this.blockName = blockName;
 };
 
 /**
@@ -64,20 +74,20 @@ var getTaskBlock = function getTaskBlock(folderId) {
  */
 function mockCreateTaskBlock() {
   var folder = new TaskFolder(123, "测试文件夹");
-  var block = new TaskBlock(1, "周四作业", folder);
+  var block = new TaskBlock(1, "周四作业", folder.id,folder.name);
 
   var course1 = new Course(1, "大屏");
   course1.taskItems.push(new TaskItem(1, "code 122"));
   course1.taskItems.push(new TaskItem(3, "测试 122"));
 
   var course2 = new Course(2, "平台");
-  course2.taskItems.push(new TaskItem(11, "另外 111" ));
+  course2.taskItems.push(new TaskItem(11, "另外 111"));
   course2.taskItems.push(new TaskItem(33, "回家 222"));
   course2.taskItems.push(new TaskItem(55, "回家 333"));
 
   block.courses.push(course1);
   block.courses.push(course2);
-  
+
   return block;
 }
 
