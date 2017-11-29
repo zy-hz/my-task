0e19c87867ad10c0b449754ac7b50b24d2759d3e
zy <yzhang@tcomchina.com>
2017-11-29 10:29:29 +0800

    c

--- client/pages/taskItemDetail/taskItemDetail.js
+++ client/pages/taskItemDetail/taskItemDetail.js
@@ -1,66 +1,13 @@
-// pages/taskItemDetail/taskItemDetail.js
-Page({
+// 页面函数，传入一个object对象作为参数
+Page(createPageObject());
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-  
-  },
+// 创建页面对象
+function createPageObject() {
+  var obj = new Object();
+  obj.data = {
+    ItemTitle: '载入作业项...',
+  };
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
-  },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  }
-})
+  return obj;
+}
