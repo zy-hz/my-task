88ea2b5e5ccf98a2311fb954f6b93020d97abe8d
zy <yzhang@tcomchina.com>
2017-12-06 10:48:34 +0800

    c

--- client/pages/introduce/introduce.js
+++ client/pages/introduce/introduce.js
@@ -12,7 +12,7 @@ Page({
   doGetUserInfo: function (options) {
     if (options.detail.userInfo != null) {
       wx.reLaunch({
-        url: '/pages/taskList/taskList',
+        url: '/pages/taskBlock/taskBlock',
       })
     }
   },
@@ -21,7 +21,11 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    wx.setNavigationBarTitle({
+      title: '授权',
+    })
     var thePage = this;
+   
     wx.getSetting({
       success(res) {
         var isAuth = res.authSetting['scope.userInfo'];
@@ -30,52 +34,4 @@ Page({
     })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
